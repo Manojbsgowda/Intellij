3cf423d0b4cbc7e7dc45ded610844fd1f512ddbb
Manojbsgowda <manojgowdas996@gmail.com>
2022-01-18 15:27:44 +0530

    assertion demo is added

---
+++ src/CheckedException.java
@@ -0,0 +1,11 @@
+import java.io.FileNotFoundException;
+
+public class CheckedException extends FileNotFoundException {
+
+    FileNotFoundException fe = new FileNotFoundException();
+
+
+    public static void main(String[] args) {
+
+    }
+}
